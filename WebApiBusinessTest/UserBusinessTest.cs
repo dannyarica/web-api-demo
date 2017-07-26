c0beb5dffc0c3a8f0bf74b2bbff2dc1402e6ccdd
Danny Arica <danny.arica@outlook.com>
2017-07-26 16:25:56 -0500

    Adding WebApiBusiness Test

---
+++ WebApiBusinessTest/UserBusinessTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiBusiness;
+
+namespace WebApiBusinessTest
+{
+    [TestFixture]
+    public class UserBusinessTest
+    {
+        [TestCase]
+        public void Test_ValidateUser_Authenticated()
+        {
+            var userBusiness = new UserBusiness();
+
+            var isAuthenticated = userBusiness.IsAuthenticated("admin", "123456");
+
+            Assert.IsTrue(isAuthenticated);
+        }
+    }
+}
