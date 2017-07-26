1970c09ac5fb7f91e1e5e661b804868a6c5c5f53
Danny Arica <danny.arica@outlook.com>
2017-07-26 15:41:48 -0500

    Adding UserBusiness

---
+++ WebApiBusiness/UserBusiness.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiBusiness
+{
+    public class UserBusiness
+    {
+        public bool IsAuthenticated(string user, string password)
+        {
+            return (user.Equals("admin") & password.Equals("123456"));
+        }
+    }
+}
