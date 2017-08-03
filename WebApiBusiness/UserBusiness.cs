c415d4253cc7766a25f60ccfdc0906ce9699fa7a
Danny Arica <danny.arica@outlook.com>
2017-08-03 16:49:34 -0500

    Adding a new method for user business

--- WebApiBusiness/UserBusiness.cs
+++ WebApiBusiness/UserBusiness.cs
@@ -1,10 +1,19 @@
-﻿namespace WebApiBusiness
+﻿using System.Collections.Generic;
+
+namespace WebApiBusiness
 {
     public class UserBusiness
     {
+
+        public Dictionary<string, string> domains = new Dictionary<string, string>() { { "admin", "admin" }, { "user", "public" } };
         public bool IsAuthenticated(string user, string password)
         {
-            return (user.Equals("admin") & password.Equals("123456")) || (user.Equals("admin") & password.Equals("admin"));
+            return (user.Equals("admin") & password.Equals("123456")) || (user.Equals("user") & password.Equals("user"));
+        }
+
+        public bool IsAuthorized(string user, string domain)
+        {
+            return domains[user].Equals(domain);
         }
     }
 }
