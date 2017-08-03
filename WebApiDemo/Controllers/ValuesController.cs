f87550388c4711e47da33051a8ac169c95cbde89
Danny Arica <danny.arica@outlook.com>
2017-08-03 17:23:16 -0500

    removing potential issues

--- WebApiDemo/Controllers/ValuesController.cs
+++ WebApiDemo/Controllers/ValuesController.cs
@@ -19,22 +19,10 @@ namespace WebApiDemo.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return id.ToString();
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
     }
 }
